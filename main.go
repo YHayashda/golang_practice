e8f9ab9361c2151ca59b0611c4b68a8f7edd7f7b
YHayashda <yoshiaki.pvt@gmail.com>
2021-05-15 17:05:10 +0900

    Add formHandlerFunc

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ import (
   "net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+  if err := r.ParseForm(); err != nil {
+    fmt.Fprintf(w, "ParseForm() err: %v", err)
+    return
+  }
+  fmt.Fprintf(w, "POST request succcessful")
+  name := r.FormValue("name")
+  address := r.FormValue("address")
+
+  fmt.Fprintf(w, "Name = %s\n", name)
+  fmt.Fprintf(w, "Address = %s\n", address)
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/hello" {
@@ -24,6 +36,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
   fileServer := http.FileServer(http.Dir("./static"))
   http.Handle("/", fileServer)
+  http.HandleFunc("/form", formHandler)
   http.HandleFunc("/hello", helloHandler)
 
   fmt.Printf("Starting server at port 8080\n")
